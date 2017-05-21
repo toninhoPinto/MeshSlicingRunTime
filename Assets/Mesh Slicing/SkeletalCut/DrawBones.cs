9552a51f2c4f0854ab49a415e9db319a11b43982
sharkspirit <Antonio.m.pinto@ist.utl.pt>
2017-05-22 00:34:53 +0100

    bone debug code

---
+++ Assets/Mesh Slicing/SkeletalCut/DrawBones.cs
@@ -0,0 +1,26 @@
+﻿// DrawBones.cs
+using UnityEngine;
+
+public class DrawBones : MonoBehaviour
+{
+    private SkinnedMeshRenderer m_Renderer;
+
+    void Start()
+    {
+        m_Renderer = GetComponentInChildren<SkinnedMeshRenderer>();
+        if (m_Renderer == null)
+        {
+            Debug.LogWarning("No SkinnedMeshRenderer found, script removed");
+            Destroy(this);
+        }
+    }
+
+    void LateUpdate()
+    {
+        var bones = m_Renderer.bones;
+        for (int i = 0; i < bones.Length; i++)
+        {
+            Debug.DrawLine(bones[i].position, bones[i].parent.position, Color.white);
+        }
+    }
+}
